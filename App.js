ac202dd7bdc0e2d06448ef7a5c75241f5f92c428
SebDev79x <sebastien.mazur@wanadoo.fr>
2022-05-09 16:27:00 +0200

    exo fini

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.wrapper/* { flexDirection: 'row', height: '50vh'  } */}>
+
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ flex: 0.9, backgroundColor: 'blue' }}></View>
+        <View style={{ flex: 0.1, backgroundColor: 'red'}}></View>
+      </View>
+
+      <View style={{ flex: 1, flexDirection: 'column' }}>
+        <View style={{ flex: 0.6, backgroundColor: 'purple' }}></View>
+        <View style={{ flex: 0.2, backgroundColor: 'yellow'}}></View>
+        <View style={{ flex: 0.2, backgroundColor: 'pink'}}></View>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  wrapper: {
+    flexDirection: 'row',
+    height: '50vh'
+
   },
 });
